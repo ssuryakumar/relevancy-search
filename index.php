0a6afe79978dbba5c23f077c9a0b89e3f60bf91b
ssuryakumar <surya091192@gmail.com>
2014-01-26 18:14:16 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css" media="all" />
+        <link rel="stylesheet" href="css/style.css" media="all" />
+    </head>
+    <body class="container">
+        <div class="row">
+            <div class="col-md-6 title">
+                <h1>RELEVANCY SEARCH</h1>
+            </div>
+            <div class="col-md-6">
+                
+            </div>
+        </div>
+        <div class="row">
+            <form action="search.php" method="post">
+                
+                <input type="text" placeholder="SEARCH" name="search" class="form-control form-inline" /><br /><br />
+                <button class="btn btn-lg btn-block btn-primary form-inline">SEARCH</button>
+            </form>
+        </div>
+    </body>
+</html>
